c5a23b151d22d0355655d545d888489dbc7eed84
Matheus Najjar <matheusnajjar@gmail.com>
2024-07-17 11:16:24 -0700

    feat: import <Login /> into App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Login from './Components/Login/Login'
 
 function App() {
 
   return (
       <div className="App">
-        Login Page
+        <Login />
       </div>
   )
 }
